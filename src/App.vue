f7f8d817917d4012fe6beee2cab8537f3cb5271f
AbelT <aelteame22@gmail.com>
2024-09-09 19:06:28 +0300

    layout change

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { computed, onMounted, watchEffect } from 'vue';
 import { useRouter } from 'vue-router';
-import { useSocket } from './composables/useSocket';
+//import { useSocket } from './composables/useSocket';
 import { useNotifications } from './store/notifications';
 import { useAuth } from './store/auth';
 
@@ -14,36 +14,36 @@ const layout = computed(
   () => `${currentRoute.value.meta.layout || defaultLayout}-layout`
 );
 
-const socket = useSocket()
-const notifications = useNotifications()
-watchEffect(async (cleanUp) => {
-  try {
-    let sock = await socket.connect()
-    socket.send('/app/toHRDI', JSON.stringify(
-      {
-        userId: auth.auth?.user?.userUuid,
-        userType: auth.auth?.user?.userType,
-      }
-    ))
+//const socket = useSocket()
+//const notifications = useNotifications()
+//watchEffect(async (cleanUp) => {
+//  try {
+//    let sock = await socket.connect()
+//    socket.send('/app/toHRDI', JSON.stringify(
+//      {
+//        userId: auth.auth?.user?.userUuid,
+//        userType: auth.auth?.user?.userType,
+//      }
+//    ))
 
-    socket.subscribe('/topic/notifyHRDI', (messgae) => {
-      console.log("hrdi");
-      console.log(messgae)
-      notifications.setNotifications(messgae)
-    })
+//    socket.subscribe('/topic/notifyHRDI', (messgae) => {
+//      console.log("hrdi");
+//      console.log(messgae)
+//      notifications.setNotifications(messgae)
+//    })
 
-    socket.subscribe('/notify/residents', (messgae) => {
-      console.log("residents");
-      console.log(messgae)
-    })
-  } catch(err) {
-    console.log(err.messgae)
-  }
+//    socket.subscribe('/notify/residents', (messgae) => {
+//      console.log("residents");
+//      console.log(messgae)
+//    })
+//  } catch(err) {
+//    console.log(err.messgae)
+//  }
 
-  return cleanUp(async () => {
-    await socket.unSub()
-  })
-}); 
+//  return cleanUp(async () => {
+//    await socket.unSub()
+//  })
+//}); 
 </script>
 
 <template>
--- src/composables/useSocket.js
+++ src/composables/useSocket.js
@@ -1,56 +1,56 @@
-const base = import.meta.env?.v_BASE_SOCKET_URL;
-const socket = new SockJS(`${base}/socket`);
+//const base = import.meta.env?.v_BASE_SOCKET_URL;
+//const socket = new SockJS(`${base}/socket`);
 
-const client = Stomp.over(socket);
-console.log(client);
+//const client = Stomp.over(socket);
+//console.log(client);
 
-export const useSocket = () => {
+//export const useSocket = () => {
   
-	function subscribe(name, cb) {
-    if (!client.connected) return console.log('You are Disconnected');
+//	function subscribe(name, cb) {
+//    if (!client.connected) return console.log('You are Disconnected');
 
-    client.subscribe(name, cb);
-  }
+//    client.subscribe(name, cb);
+//  }
 
-  function send(name, message) {
-    console.log("%cmessage", "color:green")
-    client.send(name, {}, message)
-  }
+//  function send(name, message) {
+//    console.log("%cmessage", "color:green")
+//    client.send(name, {}, message)
+//  }
 
-	async function unSub() {
-		return new Promise((res) => {
-			Object.keys((client?.subscriptions || {})).forEach(el => {
-				client.unsubscribe(el)
-			})
-			res()
-		})
-	}
+//	async function unSub() {
+//		return new Promise((res) => {
+//			Object.keys((client?.subscriptions || {})).forEach(el => {
+//				client.unsubscribe(el)
+//			})
+//			res()
+//		})
+//	}
 
-  async function connect() {
-    return new Promise((res, rej) => {
-      if(client?.connected) return res({client})
-      console.log('connecting', client.connected);
-        client.connect(
-          {},
-          function (frame) { 	
-            res({client, frame});
-            console.log('connected ', client.connected);
-            // Do something, all subscribes must be done is this callback
-            // This is needed because this will be executed after a (re)connect
-          },
-          function (error) {
-            console.error('Connection error: ' + error);
-            rej(error);
-          }
-        );
-    });
-  }
+//  async function connect() {
+//    return new Promise((res, rej) => {
+//      if(client?.connected) return res({client})
+//      console.log('connecting', client.connected);
+//        client.connect(
+//          {},
+//          function (frame) { 	
+//            res({client, frame});
+//            console.log('connected ', client.connected);
+//            // Do something, all subscribes must be done is this callback
+//            // This is needed because this will be executed after a (re)connect
+//          },
+//          function (error) {
+//            console.error('Connection error: ' + error);
+//            rej(error);
+//          }
+//        );
+//    });
+//  }
 
-  return {
-    connect,
-    subscribe,
-		client,
-		unSub,
-    send
-  };
-};
+//  return {
+//    connect,
+//    subscribe,
+//		client,
+//		unSub,
+//    send
+//  };
+//};
